219d34d7bd4b9ebf74781ad5f85352e0479d57e0
Ragini Dwivedi <ragini.dwivedi@sjsu.edu>
2021-05-09 13:23:52 -0700

    Updating cloudwatch and robotmaker api

--- routes/aws_robomaker.js
+++ routes/aws_robomaker.js
@@ -7,13 +7,31 @@ AWS.config.apiVersions = {
 };
 let robo_maker = new AWS.RoboMaker();
 let router = express.Router();
+const { exec } = require("child_process");
+
+const db = require('../utils/MysqlConfig');
+
+function execute(command, callback) {
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+            callback(null, err);
+        } else if (stderr) {
+            callback(null, stderr);
+        } else {
+            callback(stdout,null);
+        }
+    });
+}
 
 /* GET Create aws credentials. */
-router.get('/get_simulation', function(req, res, next) {
+router.get('/get_simulation/:email', function(req, res, next) {
     let params = {
-        maxResults: '10'
+        maxResults: '30'
     };
 
+    let email = req.params.email;
+
+    let results = [];
     robo_maker.listSimulationJobs(params, function(err, data) {
         if (err) {
             res.send({
@@ -21,51 +39,80 @@ router.get('/get_simulation', function(req, res, next) {
                 error: err.stack
             });
         } else {
-            result = [];
-            for (i = 0; i < data.simulationJobSummaries.length; i++){
-                if (data.simulationJobSummaries[i].robotApplicationNames[0] == "deliverychallenge_robot"){
-                    result[i] = data.simulationJobSummaries[i];
-                }
-            }
+            db.query(
+                "SELECT * FROM simulation WHERE user_email=?;", [email],(err, result) => {
+                    if (err) {
+                        res.status(500).end("Error");
+                        console.log(err);
+                    } else {
+                        let j = 0;
+                        for (let i = 0; i < data.simulationJobSummaries.length; i++){
+                            let sim = data.simulationJobSummaries[i].arn.split('/')[1];
+                            if (result.some(item => item.simulationName === sim)) {
+                                results[j] = data.simulationJobSummaries[i];
+                                j = j + 1;
+                            }
+                        }
 
-            res.send({
-                message: {
-                    simulations: result,
-                    account_id: 257034989243,
-                    error: null
+                        res.send({
+                            message: {
+                                simulations: results,
+                                account_id: 257034989243,
+                                error: null
+                            }
+                        })
+                    }
                 }
-            })
+            );
         }
     });
 });
 
 /* POST Create aws credentials. */
-router.post('/start_simulation', function(req, res, next) {
-    let sim_name = req.body.robotName;
-    res.send({
-        message: {
-            simlation_name: sim_name,
-            user: "Ragini",
-            status: "Pending",
-            data: "Simulation will be started in 5 minutes. Please login to your aws account to view the simulation",
-            start_datetime: new Date()
-        },
-        error: null
+router.post('/start_simulation/type/:type', function(req, res, next) {
+    let type = req.params.type;
+    let query = "";
+    if (type === "1" || type === 1) {
+        query = "aws robomaker create-simulation-job --max-job-duration-in-seconds 1200 --iam-role arn:aws:iam::257034989243:role/delivery-challenge-simulation-role-us-east-2 --robot-applications application=arn:aws:robomaker:us-east-2:257034989243:robot-application/deliverychallenge_robot/1618706857683,launchConfig=\"{packageName=delivery_robot_sample,launchFile=navigation_simulation.launch,streamUI=true}\" --simulation-applications application=arn:aws:robomaker:us-east-2:257034989243:simulation-application/deliverychallenge_sumulation/1618706844442,launchConfig=\"{packageName= delivery_challenge_simulation,launchFile=create_stage.launch,streamUI=true,environmentVariables={TURTLEBOT3_MODEL=burger}}\" --vpc-config assignPublicIp=true,subnets=subnet-f1c794bd,subnet-7bbe0310,subnet-99c42fe4,securityGroups=sg-080fc74a38d908d2d --region us-east-2";
+    } else {
+        query = "aws robomaker create-simulation-job --max-job-duration-in-seconds 1200 --iam-role arn:aws:iam::257034989243:role/delivery-challenge-simulation-role-us-east-2 --robot-applications application=arn:aws:robomaker:us-east-2:257034989243:robot-application/deliverychallenge_robot/1618706857683,launchConfig=\"{packageName=delivery_robot_sample,launchFile=slam_simulation.launch,streamUI=true,environmentVariables={TURTLEBOT3_MODEL=burger}}\" --simulation-applications application=arn:aws:robomaker:us-east-2:257034989243:simulation-application/deliverychallenge_sumulation/1618706844442,launchConfig=\"{packageName=delivery_challenge_simulation,launchFile=create_stage0.launch,streamUI=true,environmentVariables={TURTLEBOT3_MODEL=burger}}\" --vpc-config assignPublicIp=true,subnets=subnet-f1c794bd,subnet-7bbe0310,subnet-99c42fe4,securityGroups=sg-080fc74a38d908d2d --region us-east-2";
+    }
+
+    execute(query, function (message, error) {
+        if (error) {
+            res.send({
+                message: error,
+                error: error.message
+            });
+        } else {
+            res.send({
+                message: JSON.parse(message),
+                error: null
+            });
+        }
     });
 });
 
-/* POST Create aws credentials. */
-router.post('/stop_simulation', function(req, res, next) {
-    let sim_name = req.body.robotName;
-    res.send({
-        message: {
-            simlation_name: sim_name,
-            user: "Ragini",
-            status: "Stoping",
-            data: "Simulation will be started in 5 minutes. Please login to your aws account to view the simulation",
-            stop_datetime: new Date()
-        },
-        error: null
+/* POST stop simulation. */
+router.post('/stop_simulation/:simulationId', function(req, res, next) {
+    let sim_Id = req.params.simulationId;
+    let simulation_name = "arn:aws:robomaker:us-east-2:257034989243:simulation-job/" + sim_Id;
+
+    let query = "aws robomaker cancel-simulation-job --job " + simulation_name + " --region us-east-2";
+    execute(query, function (message, error) {
+        if (error) {
+            res.send({
+                message: error,
+                simulation_id: sim_Id,
+                error: error.message
+            });
+        } else {
+            res.send({
+                message: "Simulation stopped successfully",
+                simulation_id: sim_Id,
+                error: null
+            });
+        }
     });
 });
 
@@ -89,13 +136,17 @@ router.get('/list_robots', function(req, res, next) {
                 }
             }
 
+            let filteredResult = result.filter(function (el) {
+                return el != null;
+            });
+
             res.send({
                 message: {
-                    simulations: result,
+                    simulations: filteredResult,
                     account_id: 257034989243,
                     error: null
                 }
-            })
+            });
         }
     });
 });
